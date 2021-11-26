35bbb0943a235209ca10d99bd2a0d04601ad6231
juliancamy.81@ <juliancamy.81@gmail.com>
2021-11-26 18:12:56 -0500

    Gatsby tutorial parte 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+
+    return (
+        <Layout pageTitle="Sobre mi">
+            <p>Hola esto es un demo de gatsby usando react :D</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
